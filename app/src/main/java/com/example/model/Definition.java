4ff4fdd9e1ac086c72889e08d602bcbbd78bd554
nazeermazeer <234304154+nazeermazeer@users.noreply.github.com>
2026-07-05 02:01:06 -0600

    update model

--- app/src/main/java/com/example/model/Definition.java
+++ app/src/main/java/com/example/model/Definition.java
@@ -3,26 +3,26 @@ package com.example.model;
 import java.util.List;
 
 public class Definition {
-    private List<String> term;
+    private List<String> signature;
     private String definition;
 
     public Definition() {}
 
-    public Definition(List<String> terms, String def) {
-        this.term = terms;
+    public Definition(List<String> signatures, String def) {
+        this.signature = signatures;
         this.definition = def;
     }
 
-    public void setTerm(List<String> newterm){
-        this.term = newterm;
+    public void setSignature(List<String> newsignature){
+        this.signature = newsignature;
     }
 
     public void setDefinition(String newdef){
         this.definition = newdef;
     }
 
-    public List<String> getTerm(){
-        return this.term;
+    public List<String> getSignature(){
+        return this.signature;
     }
 
     public String getDefinition() {
