cd5b6da2a067967d8e5e05172ab5db3263d408cf
nazeermazeer <234304154+nazeermazeer@users.noreply.github.com>
2026-07-09 21:28:57 -0600

    no outside borders bc i am boring

--- app/src/main/java/com/example/Main.java
+++ app/src/main/java/com/example/Main.java
@@ -37,7 +37,7 @@ public class Main extends ToolkitApp {
 
     @Override
     protected Element render() {
-        return panel(PATH.getFileName().toString(), panel(document).borderType(BorderType.NONE), panel(searchbar));
+        return panel(PATH.getFileName().toString(), panel(document).borderType(BorderType.NONE), panel(searchbar)).borderType(BorderType.NONE);
         // return column(panel(document).borderType(BorderType.NONE).length(10), spacer(), panel(searchbar));
 
     }
