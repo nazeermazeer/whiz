27051d35c79133b28f68605ae44349d00c1c2654
nazeermazeer <234304154+nazeermazeer@users.noreply.github.com>
2026-08-02 13:05:48 -0600

    lint indexer

--- app/src/main/java/com/example/Indexer.java
+++ app/src/main/java/com/example/Indexer.java
@@ -2,6 +2,7 @@ package com.example;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
@@ -12,6 +13,7 @@ import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.index.StoredFields;
 import org.apache.lucene.queryparser.classic.MultiFieldQueryParser;
+import org.apache.lucene.queryparser.classic.ParseException;
 import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.Query;
 import org.apache.lucene.search.ScoreDoc;
@@ -20,100 +22,105 @@ import org.apache.lucene.store.ByteBuffersDirectory;
 
 import java.io.IOException;
 import java.nio.file.Path;
-import java.io.File;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+
 import com.example.model.Definition;
 
 public class Indexer {
     public record IndexResult(ByteBuffersDirectory directory, StandardAnalyzer analyzer) {}
-    public record SearchResult(String[] location, String[] term, String[] definition) {}
+    public record SearchResult(String[] location, String[] term, String[] definition) {
+        @Override
+        public boolean equals(Object o) {
+            if (o == null || getClass() != o.getClass()) return false;
+            SearchResult result = (SearchResult) o;
+            return Arrays.equals(location, result.location) && 
+                Arrays.equals(term, result.term) && 
+                Arrays.equals(definition, result.definition);
+        }
 
-    private List<Definition> entries;
+        @Override
+        public int hashCode() {
+            int result = Arrays.hashCode(location);
+            result = 31 * result + Arrays.hashCode(term);
+            result = 31 * result + Arrays.hashCode(definition);
+            return result;
+        }
+
+        @Override
+        public String toString() {
+            return "SearchResult[" +
+                "location=" + Arrays.toString(location) + ", " +
+                "term=" + Arrays.toString(term) + ", " +
+                "definition=" + Arrays.toString(definition) + 
+                "]";
+        }
+    }
+
     private IndexResult index;
+    private static final String DEFSTR = "definition";
 
-    public void indexEntries() throws Exception {
-        entries = readJSON(Path.of("app/src/main/java/com/example/entries.json").toFile());
+    public void indexEntries() throws IOException {
+        List<Definition> entries = readJSON();
         index = readIndex(entries);
     }
 
-    public List<SearchResult> searchTerm (String search) throws Exception {
+    public List<SearchResult> searchTerm (String search) throws IOException, ParseException {
         return search(search, index.directory, index.analyzer);
     }
 
-    private List<Definition> readJSON(File filePath) throws IOException {
+    private List<Definition> readJSON() throws IOException {
         ObjectMapper mapper = new ObjectMapper();
 
-        List<Definition> entries = mapper.readValue(
+
+        return mapper.readValue(
                 Path.of("app/src/main/java/com/example/entries.json").toFile(),
                 new TypeReference<List<Definition>>() {}
         );
-
-        return entries;
     }
 
     private IndexResult readIndex(List<Definition> entries) throws IOException {
         ByteBuffersDirectory directory = new ByteBuffersDirectory();
         StandardAnalyzer analyzer = new StandardAnalyzer();
 
-        try (IndexWriter writer = new IndexWriter(
-                directory,
-                new IndexWriterConfig(analyzer))) {
-
+        try (IndexWriter writer = new IndexWriter(directory, new IndexWriterConfig(analyzer))) {
             for (Definition def : entries) {
-
                 Document doc = new Document();
-
                 doc.add(new StringField("location", def.getLocation(), Field.Store.YES));
 
-                // Add every term
                 for (String term : def.getSignature()) {
-                    doc.add(new TextField(
-                            "term",
-                            term,
-                            Field.Store.YES
-                    ));
+                    doc.add(new TextField("term", term, Field.Store.YES));
                 }
 
-                // Add definition
-                doc.add(new TextField(
-                        "definition",
-                        def.getDefinition(),
-                        Field.Store.YES
-                ));
-
+                doc.add(new TextField(DEFSTR, def.getDefinition(), Field.Store.YES));
                 writer.addDocument(doc);
             }
-
             writer.commit();
         }
 
         return new IndexResult(directory, analyzer);
     }
 
-    public static void main(String[] args) throws Exception {
-
-    }
 
 
+    private List<SearchResult> search(String search, ByteBuffersDirectory directory, StandardAnalyzer analyzer) throws IOException, ParseException {
+        List<SearchResult> searchresults = new ArrayList<>();
+        DirectoryReader reader = DirectoryReader.open(directory);
+        IndexSearcher searcher = new IndexSearcher(reader);
+        MultiFieldQueryParser parser = new MultiFieldQueryParser(new String[]{"term", DEFSTR}, analyzer);
 
-    private List<SearchResult> search(String search, ByteBuffersDirectory directory, StandardAnalyzer analyzer) throws Exception {
+        Query query = parser.parse(search);
+        TopDocs results = searcher.search(query, 10);
+        StoredFields storedFields = reader.storedFields();
 
-    List<SearchResult> searchresults = new ArrayList<>();
-    DirectoryReader reader = DirectoryReader.open(directory);
-    IndexSearcher searcher = new IndexSearcher(reader);
-    MultiFieldQueryParser parser = new MultiFieldQueryParser(new String[]{"term", "definition"}, analyzer);
+        for (ScoreDoc hit : results.scoreDocs) {
+            Document doc = storedFields.document(hit.doc);
 
-    Query query = parser.parse(search);
-    TopDocs results = searcher.search(query, 10);
-    StoredFields storedFields = reader.storedFields();
+            searchresults.add(new SearchResult(doc.getValues("location"), doc.getValues("term"), doc.getValues(DEFSTR)));
 
-    for (ScoreDoc hit : results.scoreDocs) {
-        Document doc = storedFields.document(hit.doc);
+            }
 
-        searchresults.add(new SearchResult(doc.getValues("location"), doc.getValues("term"), doc.getValues("definition")));
-
-        }
-    return searchresults;
+        return searchresults;
     }
 }
